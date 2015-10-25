b813dc6308c7560439109f1d004a97d809741ca8
Fernando Borretti <eudoxiahp@gmail.com>
2015-10-25 19:25:43 -0300

    Add tests

---
+++ t/austral-env.lisp
@@ -0,0 +1,21 @@
+(in-package :cl-user)
+(defpackage austral-env-test
+  (:use :cl :fiveam)
+  (:export :run-tests))
+(in-package :austral-env-test)
+
+(def-suite tests
+  :description "austral-env tests.")
+(in-suite tests)
+
+(defparameter +directory+
+  (asdf:system-relative-pathname :austral-env #p"t/ql/"))
+
+(test get-installer
+  (let ((manager (make-instance 'austral-env.ql:quicklisp-manager
+                                :directory +directory+)))
+    (finishes
+     (austral-env.ql:download-quicklisp-installer manager))))
+
+(defun run-tests ()
+  (run! 'tests))
